f011446a4b3e03c3a4e811f867005899e364fce1
M-H-Riad <meshkathasanriad@gmail.com>
2024-10-12 01:30:01 +0600

    url fixed

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -29,13 +29,6 @@
   <!-- Template Main CSS File -->
   <link href="{{asset('')}}assets/css/style.css" rel="stylesheet">
 
-  <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Updated: Apr 20 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
@@ -44,7 +37,7 @@
   <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="{{route('/')}}" class="logo d-flex align-items-center">
+      <a href="{{url('/')}}" class="logo d-flex align-items-center">
         <img src="{{ asset('storage/' . settings('logo')) }}" alt="">
         <span class="d-none d-lg-block">{{ settings('site_name', 'VAIL RESORTS') }}</span>
       </a>
@@ -104,17 +97,6 @@
 
   <main id="main" class="main">
 
-    {{-- <div class="pagetitle">
-      <h1>Dashboard</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-          <li class="breadcrumb-item active">Dashboard</li>
-        </ol>
-      </nav>
-    </div><!-- End Page Title --> --}}
-
-
     {{-- Show Success and Failure Message --}}
     @if(session('success'))
         <div class="alert alert-success alert-dismissible fade show" role="alert" id="dismissable-alert">
@@ -149,11 +131,11 @@
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
-  <footer id="footer" class="footer">
+  {{-- <footer id="footer" class="footer">
     <div class="copyright">
       &copy; Copyright <strong><span> </span></strong>. All Rights Reserved
     </div>
-  </footer><!-- End Footer -->
+  </footer><!-- End Footer --> --}}
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
